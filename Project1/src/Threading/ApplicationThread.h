1622dda3029474391da31435038fda34b165fe4c
mohamedibrahim155 <63718196+mohamedibrahim155@users.noreply.github.com>
2024-03-08 17:42:38 -0500

    initial commit

---
+++ Project1/src/Threading/ApplicationThread.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "ThreadStruct.h"
+
+extern ApplicationThread* applicationThreadExtern = new ApplicationThread();
+
+
+DWORD WINAPI UpdateApplicationThread(LPVOID lpParameter)
+{
+
+	ApplicationThread* threadPointer = (ApplicationThread*)lpParameter;
+
+	double lastFrameTime = glfwGetTime();
+	double timeStep = 0.0;
+	DWORD sleepTime_ms = 1;
+
+	InitializeCriticalSection(&threadPointer->cs);
+
+	while (threadPointer->isActive)
+	{
+		if (threadPointer->isThreadActive)
+		{
+			double currentTime = glfwGetTime();
+			double deltaTime = currentTime - lastFrameTime;
+			lastFrameTime = currentTime;
+
+			timeStep += deltaTime;
+
+
+
+			
+				//threadPointer->manager->Update(deltaTime);
+				threadPointer->entityManager->Update(deltaTime);
+				threadPointer->physicsEngine->Update(deltaTime);
+
+
+				
+
+			Sleep(threadPointer->sleepTime);
+		}
+
+	}
+
+
+	return 0;
+
+}
+
+void InitializeApplicationThread(float time)
+{
+	
+	applicationThreadExtern->desiredUpdateTime = time;
+	applicationThreadExtern->physicsEngine = &PhysicsEngine::GetInstance();
+	applicationThreadExtern->manager = &ThreadManager::Getinstance();
+	applicationThreadExtern->entityManager = &EntityManager::GetInstance();
+	applicationThreadExtern->panelmanager = &PanelManager::GetInstance();
+	applicationThreadExtern->isActive = true;
+	applicationThreadExtern->sleepTime = 1;
+
+	applicationThreadExtern->threadHandle = CreateThread(
+		NULL,						
+		0,							
+		UpdateApplicationThread,		
+		(void*)applicationThreadExtern,			
+		0,						
+		&applicationThreadExtern->ThreadId);		
+
+	// SoftBody Thread
+}
---
+++ Project1/src/Threading/ThreadStruct.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Windows.h>
+#include "../Physics/PhysicsEngine.h"
+#include "ThreadManager.h"
+#include "../EntityManager/EntityManager.h"
+#include "../ImGui/PanelManager.h"
+
+
+struct ApplicationThread // SoftBody Struct Infor
+{
+	PhysicsEngine* physicsEngine;
+    ThreadManager* manager;
+    EntityManager* entityManager;
+    PanelManager* panelmanager;
+
+
+    double desiredUpdateTime = 0.0;  
+    bool isThreadActive = false;	
+    bool isActive = true;
+    DWORD sleepTime;	
+
+    DWORD ThreadId = 0;
+    HANDLE threadHandle = 0;
+    CRITICAL_SECTION cs;
+
+};
